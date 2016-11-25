fe89166fb9eef3e7a97b669308271b0127950006
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-25 12:36:07 +0100

    Authentication little regactor

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    return this.get('session').fetch().catch(function() {});
+    return this.get('session').fetch().catch(()=> null);
   },
   actions: {
     signIn: function(provider) {
