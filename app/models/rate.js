9626e8f525b74c8f5bec8a881b071f3ab4ae4cee
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-28 09:26:54 +0100

    Log/create user.. little refactor

--- app/models/rate.js
+++ app/models/rate.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  user: DS.belongsTo('user'),
+  value: DS.attr('number')
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.get('session').fetch().catch(()=> null);
+  model() {
+    return Ember.RSVP.hash({
+      users: this.store.findAll('user')
+    });
   },
-  actions: {
-    signIn: function(provider) {
-      this.get('session').open('firebase', { provider: provider}).then(function(data) {
-        console.log(data.currentUser);
-      });
-    },
-    signOut: function() {
-      this.get('session').close();
-    }
+  beforeModel: function() {
+    return this.get('session').fetch().catch(() => null);
   }
 });
+
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   beforeModel: function() {
+//     return this.get('session').fetch().catch(()=> null);
+//   },
+//   actions: {
+//     signIn: function(provider) {
+//       this.get('session').open('firebase', { provider: provider}).then(function(data) {
+//         console.log(data.currentUser);
+//       });
+//     },
+//     signOut: function() {
+//       this.get('session').close();
+//     }
+//   }
+// });
