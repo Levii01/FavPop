e5c2b2ad7e25ed9f458ab0fc1f3089714ba8bd25
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-28 11:12:39 +0100

    Add like to person

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  store: Ember.inject.service(),
+  actions: {
+    addRating(person) {
+      this.get('store').createRecord('rate', {user: person, value: 1})
+        .save();
+
+      person.save();
+    }
+  }
 });
