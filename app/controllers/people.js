c3a122f272ee8bb11f2c7500021045ad67a6d826
Miłosz Sędziak <m.sedziak01@gmail.com>
2016-11-28 12:02:54 +0100

    Controler and ruls refactor. Now you can't like your profile. :^)

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,13 +1,30 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
-  store: Ember.inject.service(),
   actions: {
     addRating(person) {
-      this.get('store').createRecord('rate', {user: person, value: 1})
-        .save();
-
-      person.save();
+      let newRecord = this.get('store')
+        .createRecord('rate', {user: person, value: 1});
+      newRecord
+        .save()
+        .then(() => person.save())
+        .catch(() => newRecord.deleteRecord());
     }
   }
 });
+
+// import Ember from 'ember';
+//
+// export default Ember.Controller.extend({
+//   store: Ember.inject.service(),
+//   actions: {
+//     addRating(person) {
+//       let newRecord = this.get('store')
+//         .createRecord('rate', {user: person, value: 1});
+//
+//     newRecord
+//     .save()
+//     .then(() => person.save())
+//     .catch(() => newRecord.deleteRecord());
+//     }
+//   }
+// });
